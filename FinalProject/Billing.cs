557f0e9319652876167873ac5bf05f129e0aca7a
Justin200104 <57876808+Justin200104@users.noreply.github.com>
2020-11-25 11:43:05 -0500

    Update Billing.cs

--- FinalProject/Billing.cs
+++ FinalProject/Billing.cs
@@ -8,5 +8,37 @@ namespace FinalProject
 {
     class Billing
     {
+        int buyerID;
+        int orderID;
+        int customerID;
+        int itemID;//idk if this is needed
+        bool payment;
+
+
+        public void VerifyPayment(int buyerID, int orderID, int customerID)
+        {
+            if (payment == true)
+            {
+                Receipt(buyerID, orderID, customerID);
+            }
+            else
+            {
+                Receipt(buyerID, orderID, customerID);
+            }
+        }
+
+        public string Receipt(int buyerID, int orderID, int customerID)
+        {
+            string receiptLog ="";
+            if (payment == true)
+            {
+                receiptLog = "BuyerID: " + buyerID.ToString() + " OrderID: " + orderID.ToString() + " CustomerID: " + customerID.ToString();
+            }
+            else
+            {
+                receiptLog = "Payment Decline";
+            }
+            return receiptLog;
+        }
     }
 }
