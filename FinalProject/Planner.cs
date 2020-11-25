334c01ba1d6c3c98130369f967ec0620a2ea4215
Justin200104 <57876808+Justin200104@users.noreply.github.com>
2020-11-25 15:48:21 -0500

    Commented the Planner.cs class with doxygen style comments

--- FinalProject/Planner.cs
+++ FinalProject/Planner.cs
@@ -6,11 +6,35 @@ using System.Threading.Tasks;
 
 namespace FinalProject
 {
+    /// 
+    /// \class Planner
+    ///
+    /// \brief The purpose of this class is to realistically model the Planner for the TMS system.
+    ///	
+    ///		NAME: Planner
+    ///		PURPOSE : The Planner class has been created to model the Planner.
+    ///
+    /// Within the class definition 
+    /// - int plannerID	- models the ID of a plannerID.
+    /// - int orderID   - models the ID of the order.
+    /// - string address - models the ID of the address.
+    ///
+    /// \author  <i>Input name here</i>
+    ///
     class Planner
     {
         int plannerID;
         int orderID;
         string address; //(trip)
+
+        ///
+        ///		\brief Called to select a carrier.
+        ///		\details <b>Details</b>
+        ///     
+        ///		This Methods returns the carrier that has been selected.
+        /// 
+        ///		\return void.
+        ///
         public string SelectCarrier()
         {
             return "s";
