d06f7fe0632857c29557e3955ca9ed868f9982e3
Justin200104 <57876808+Justin200104@users.noreply.github.com>
2020-11-25 16:28:14 -0500

    Heres a link to understand the unit tests

--- TmsUnitTest/UnitTest1.cs
+++ TmsUnitTest/UnitTest1.cs
@@ -3,6 +3,8 @@ using System;
 
 namespace TmsUnitTest
 {
+    //https://docs.microsoft.com/en-us/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2019
+    //link to understand unit tests
     [TestClass]
     public class UnitTest1
     {
