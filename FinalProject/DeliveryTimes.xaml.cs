55ebe5529fe77d6c976ac70dbcc5909f862a3c76
NikProgramming <NikProgramming@users.noreply.github.com>
2020-11-25 15:15:07 -0500

    Comment Delivery time

--- FinalProject/DeliveryTimes.xaml.cs
+++ FinalProject/DeliveryTimes.xaml.cs
@@ -14,15 +14,46 @@ using System.Windows.Shapes;
 
 namespace FinalProject
 {
-    /// <summary>
-    /// Interaction logic for DeliveryTimes.xaml
-    /// </summary>
+
+    /// 
+    /// \class DeliveryTimes
+    ///
+    /// \brief The purpose of this class is to realistically model the Delievery times for the TMS system.
+    ///	
+    ///		NAME: DeliveryTimes
+    ///		PURPOSE : The DeliveryTimes class has been created to 
+    ///
+    /// Within the class definition 
+    /// - N/A
+    ///
+    /// \author  <i>Justin Langevin</i>
+    ///
     public partial class DeliveryTimes : Window
     {
+        ///
+        ///		\brief Started when Delivery date is clicked
+        ///		\details <b>Details</b>
+        ///     
+        ///		This Method initializes the DeliveryTimes area and displays all the buttons and designs
+        /// 
+        ///		\return N/A.
+        ///
         public DeliveryTimes()
         {
             InitializeComponent();
         }
+
+
+        ///
+        ///		\brief Called to create the back button for the DeliveryTimes.
+        ///		\details <b>Details</b>
+        ///		\param sender - <b>object</b> - Represents the object that is calling the method.
+        ///     \param e - <b>RoutedEventArgs</b> - Represents the item thats being sent in.
+        ///
+        ///		This Method returns the user to the main window.
+        /// 
+        ///		\return N/A.
+        ///
         private void Button_Click1(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
