39d282341590a7ab30ff685965f1d9b43179bd87
Justin200104 <57876808+Justin200104@users.noreply.github.com>
2020-11-26 11:39:31 -0500

    Made the Doxygen Main Paige for the project.

--- FinalProject/TmsDoxygenMainPage.cs
+++ FinalProject/TmsDoxygenMainPage.cs
@@ -1,44 +1,48 @@
-﻿/// \mainpage Shapes Project   
+﻿/// \mainpage TMS Final Project   
 /// \image html 1_-i9M1D3PQv8VdZ9BgL90Kg
 ///
 ///
 /// \section intro Program Introduction
-/// This set of assignments (A-04, A-07 and A-08) are meant to give you practice (once again) at developing class definitions from scratch, but in this second installment, you will be
-///	adding some overloaded operators to your existing classes.  The set of activities are broken down into a 3 stages. 
-/// (<i>DOxygen commenting styles</i>) (<i>Repository work</i>) (<i>adding overloaded operators and programing</i>). 
-///
-///
-/// The Shape project features to note are:
+/// This Program is used for the TMS (<i>Transportation Management System</i>). TMS is a critical part of the shipping and logistics industry. 
+/// Allowing for the purchasing, scheduling, monitoring and billing of organizations who desire to have cargo shipped via truck or train.
 /// 
-/// - Inheritance between the Shape.h Circle.h and Square.h <b>classes<b>
-/// - how the <b>pure-virtual</b> functions are incorporated throughout the program to calculate <i>area</i> <i>perimeter</i> and <i>OverallDimension</i>.
-/// - how constructors check for valid input in the Shapes.cpp file
-/// - how <b>methods</b> are used to output the information to the user without loosing scope.
-/// - how <b>Assessors</b> don't break encapsulation.
-/// - Overloaded operators.
 ///
-/// The 3 pure-virtual functions 
-/// -# <i>virtual float Perimeter(void) = 0;</i>
-/// -# <i>virtual float Area(void) = 0;</i> 
-/// -# <i>virtual float OverallDimension(void) = 0;</i> 
 ///
+/// The TMS project features to note are:
 /// 
+/// - <b>Account System</b>: Manages user accounts.
+/// - <b>Billing System</b>: Checks to see if users have payed and generates a receipt.
+/// - <b>Buyer System</b>: Allows the buyer to request contracts and create orders.
+/// - <b>Carrier System</b>: Sets up what carrier is currently in use for the TMS specific orders.
+/// - <b>Invoice System</b>: Creates an invoice for the users.
+/// - <b>Order System</b>: Allows for customer to order items.
+/// - <b>Planner System</b>: Allows for the planner to select a carrier.
+/// 
+/// The UI Section
+/// 
+/// - <b>MainWindow</b>: The main window of the program that display the delivery type, login, market place, and register for the TMS program.
+/// - <b>Login</b>: used to allows <b>admins</b> to log into the program.
+/// - <b>Register</b>: allows for new <b>admins</b> to be created. 
+/// - <b>DeliveryTimes</b>: display the time the order is supposed to arrive.
+/// - <b>MarketPlace</b>: use to allow <b>admins</b> to get new contracts.
+/// 
+///  
 ///
 /// <hr>
 /// \section The Requirements 
-/// - <A target=blank HREF="https://conestoga.desire2learn.com/d2l/le/content/320026/viewContent/6396061/View"> A-07: Shapes - Adding Operators</A>
+/// - <A target=blank HREF="https://conestoga.desire2learn.com/d2l/le/content/375951/viewContent/8105487/View"> SQ-TermProjectOverView -TMSv2</A>
 ///
 ///
 /// 
 /// <hr>
 /// \section version Current version of the Shape Project :
 /// <ul>
-/// <li>\author    <b><i>Justin Langevin</i></b></li>
+/// <li>\author    <b><i>Justin Langevin</i></b> <b><i>Nikola Ristic</i></b> <b><i>Troy Hill</i></b> <b><i>Josiah Rehkopf</i></b></li>
 /// <li>\version   1.00.01</li>
-/// <li>\date      2020-03-21</li>
-/// <li>\pre       In order to use the <i>Shape Project</i> - you must first start with a shape</li>
-/// <li>\warning   Improper use of the <i>Shape Project</i> may result in the program throwing warnings</li>
-/// <li>\copyright Shapes-Expert Team</li>
+/// <li>\date      2020-11-26</li>
+/// <li>\pre       In order to use the <i>TMS</i> - you must first start by downloaded the .exe</li>
+/// <li>\warning   Improper use of the <i>TMS</i> may result in the program throwing warnings</li>
+/// <li>\copyright SET Friends team</li>
 /// </ul>
 ///
 
